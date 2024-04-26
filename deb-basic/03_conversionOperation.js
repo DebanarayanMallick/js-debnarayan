f5880bd1d9d603605cc5aaf6b2d77583dd6e94e9
DebanarayanMallick <95491230+DebanarayanMallick@users.noreply.github.com>
2024-04-26 06:14:25 +0000

    conversionOperation

--- deb-basic/03_conversionOperation.js
+++ deb-basic/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc";
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 let age = null;
 
@@ -15,8 +15,8 @@ let age = null;
 let isLoginId = 1;
 
 let userLoginId = Boolean(isLoginId);
-console.log(typeof userLoginId);
-console.log(userLoginId);
+// console.log(typeof userLoginId);
+// console.log(userLoginId);
 
 // 1 => true, 0 => false
 // "" => false
@@ -24,5 +24,49 @@ console.log(userLoginId);
 
 let someNumber = 55;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************** Operations **********************************//
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let name = "Debanarayan";
+let surName = " Mallick";
+let fullName = name + surName;
+// console.log(fullName);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + 2 + "3");
+// console.log("1" + 2 + 3);
+
+// console.log((3+4) * 5 % 3);
+// console.log(true);
+// console.log(+true);
+// // console.log(true+); // error 
+// console.log(+"");
+
+let num1 , num2 , num3;
+num1 = num2 = num3 = 2 + 2 ;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
+
+let game = 20 ;
+++game;
+// console.log(game);
